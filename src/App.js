33898eca05faedc7815cf193add7cbde673955b0
Mohamed ShamsEldeen <mmm.emad64@gmail.com>
2022-12-01 23:09:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home  from './pages/Home';
+// import Articles from './pages/Articles';
+// import BeingHere from './pages/BeingHere';
+// import Careers from './pages/Careers';
+// import Contact from './pages/Contact';
+// import Privacy from './pages/Privacy';
+// import WhoWeAre from './pages/WhoWeAre';
+// import WhatWeDo from './pages/WhatWeDo';
+// import Work from './pages/Work';
+// import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <>
+      {/* <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route       path="/articles" element={<Articles />}/>
+          <Route       path="/being-here" element={<BeingHere />}/>
+          <Route       path="/careers" element={<Careers />}/>
+          <Route       path="/contact" element={<Contact />}/>
+          <Route       path="/privacy" element={<Privacy />}/>
+          <Route       path="/WhoWeAre" element={<WhoWeAre />}/>
+          <Route       path="/what-we-do" element={<WhatWeDo />}/>
+          <Route       path="/work" element={<Work />}/>
+        </Routes>
+      </Router> */}
+    </>
+  );
+}
+
+export default App;
